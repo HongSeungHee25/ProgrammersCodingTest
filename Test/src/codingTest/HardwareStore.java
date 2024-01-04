1c2b7ca1c863d50bb1c0a464ee085eb6e9ae3ba9
홍승희 <seung0476@gmail.com>
2024-01-04 18:25:18 +0900

    코딩테스트 최종본 업데이트

--- Test/src/codingTest/HardwareStore.java
+++ Test/src/codingTest/HardwareStore.java
@@ -1,6 +1,7 @@
 package codingTest;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class HardwareStore {
@@ -10,7 +11,7 @@ public class HardwareStore {
 	        System.out.print("가게의 부품 개수를 입력하세요: ");
 	        int n = scanner.nextInt();
 	        int[] storeParts = new int[n];
-	        System.out.println("가게의 부품 번호를 입력하세요: ");
+	        System.out.print("가게의 부품 번호를 입력하세요: ");
 	        for (int i = 0; i < n; i++) {
 	            storeParts[i] = scanner.nextInt();
 	        }
@@ -18,12 +19,12 @@ public class HardwareStore {
 	        System.out.print("손님이 요청한 부품 종류 수를 입력하세요: ");
 	        int m = scanner.nextInt();
 	        int[] customerParts = new int[m];
-	        System.out.println("손님이 요청한 부품 번호를 입력하세요: ");
+	        System.out.print("손님이 요청한 부품 번호를 입력하세요: ");
 	        for (int i = 0; i < m; i++) {
 	            customerParts[i] = scanner.nextInt();
 	        }
 
-	        HashSet<Integer> storeSet = new HashSet<>();
+	        List<Integer> storeSet = new ArrayList<>();
 
 	        // 가게의 부품 번호를 HashSet에 추가
 	        for (int part : storeParts) {
@@ -33,9 +34,9 @@ public class HardwareStore {
 	        // 손님이 요청한 부품 번호를 하나씩 확인하여 가게에 있는지 여부를 출력
 	        for (int part : customerParts) {
 	            if (storeSet.contains(part)) {
-	                System.out.println("yes");
+	                System.out.print("yes ");
 	            } else {
-	                System.out.println("no");
+	                System.out.print("no ");
 	            }
 	        }
 
